18af753ade3002b30c864e95b64e6c8e759136ed
guilhermeaam <guilhermeafonso80@gmail.com>
2017-09-05 11:51:53 -0300

    fim de aula

---
+++ aula20170905/seq1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+	int a, l;
+	printf("Digite o lado do quadrado ");
+	scanf("%d",&l);
+	a=l*l;
+	printf("o valor da area e: %d",a);
+	return 0;	
+}
---
+++ aula20170905/seq2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+	int a, b, h;
+	printf("Digite a base e a altura do paralelograma");
+	scanf("%d",&b);
+	scanf("%d",&h);
+	a=b*h;
+	printf("o valor da area e: %d",a);
+	return 0;	
+}
